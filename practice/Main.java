fac1718ced95a3e7e310cb0bd148a385880e2c31
PARKHYEONJUNN <a45461640@mju.ac.kr>
2026-07-20 05:00:55 +0900

    feat: FK를 자바 객체 참조로 흉내내기

--- practice/Main.java
+++ practice/Main.java
@@ -1,22 +1,12 @@
-
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        // 1. 유저 객체 생성 및 정보 세팅
-        User user = new User();
-        user.name = "홍길동";
-        user.email = "hong@test.com";
+        // 가게(Store) 객체 생성
+        Store store = new Store(1L, "가게이름a", "서울시 강남구");
 
-        // 2. 찜할 영화 객체들 생성
-        Movie movie1 = new Movie("인셉션", "SF", 2010);
-        Movie movie2 = new Movie("타이타닉", "로맨스", 1997);
+        // 미션(Mission) 객체 생성 (가게 객체를 인자로 넘겨 연결해 줍니다)
+        Mission mission = new Mission(1L, "12,000원 이상 식사", store);
 
-        // 3. 유저 필드 메서드를 사용하여 영화 찜하기
-        user.addFavorite(movie1);
-        user.addFavorite(movie2);
-
-        // 4. 유저 필드 메서드로 찜 목록 바로 출력하기
-        user.printFavorites();
+        // 결과 출력 확인: "가게이름a"가 정상 출력되어야 합니다.
+        System.out.println(mission.getStore().getName());
     }
 }
